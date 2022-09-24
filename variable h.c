91e1e00a86c89d20e82b3af11d7ca16911d7882d
anilbegar17 <114311958+anilbegar17@users.noreply.github.com>
2022-09-25 01:21:17 +0530

    Add files via upload

---
+++ variable h.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	 int a=4;
+	 float b=9.5;
+	 char c='a';
+	 int d=35;
+	 int e=45+4;
+	 
+	 printf("%d\n", a);
+	  printf("%f\n", b);
+	   printf("%c\n", c);
+	    printf(" sum of a and d is %d\n", a-d);
+	     printf(" sum of a and d is  %d\n",e);
+	     
+	     return 0;
+	 }
